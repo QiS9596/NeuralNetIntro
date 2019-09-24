cf51dfd9fe4fd188caae15c69a04f076908f5ecc
Qi Song <rolo_lamperouge@foxmail.com>
2019-09-23 23:45:11 -0400

    complete neural net tools

--- NeuralNets.py
+++ NeuralNets.py
@@ -62,6 +62,14 @@ class Neuron:
             return self.delta
 
     def update(self, lr=0.5, momentum=0.9):
+        """
+        update function of neurons, update the weight based on pre-calculated delta value
+        - should be invoked after back_propagate is invoked
+        - should be invoked by the governing Layer object
+        :param lr: learning rate of backprop
+        :param momentum: momentum term
+        :return:
+        """
         update = lr * self.delta * self.augmented_input
         new_weight = self.weight + update + momentum * self.last_update
         self.last_update = update
@@ -69,6 +77,11 @@ class Neuron:
 
 
 class Layer:
+    """
+    General layer object
+    each layer will keep track of the previous layer and next layer so that recursive function call could be implemented
+    """
+
     def __init__(self, n_neurons):
         self.n_neurons = n_neurons
         self.next_layer = None
@@ -82,7 +95,18 @@ class Layer:
 
 
 class DenseLayer(Layer):
+    """
+    Dense layer (Fully-connected Feed-forward layer)
+    """
+
     def __init__(self, n_neurons, previous_layer, isOutput=False, activation_function=sigmoid):
+        """
+        Initialize a DenseLayer object
+        :param n_neurons: number of neurons of current layer
+        :param previous_layer: previous layer, whose n_neurons will be used as input dimension for this layer
+        :param isOutput: bool value to indicate if this layer is a output layer
+        :param activation_function: activation function, function object
+        """
         Layer.__init__(self, n_neurons)
         self.isOutput = isOutput
         self.next_layer = None
@@ -95,6 +119,14 @@ class DenseLayer(Layer):
                 Neuron(n_dim=self.previous_layer.n_neurons, activation_func=activation_function, layer=self))
 
     def forward_propagate(self, previous_layer_output):
+        """
+        forward_propagate method
+        For DenseLayer object, this method should only be invoked by another Layer object as an action of recursive call
+        Calculate the output by first applying the dot-product of previous_payer_output with each neuron's
+        weight vector and apply activation function. The output of each neuron is concatenated and passed to next layer
+        :param previous_layer_output: np.array with dimensionality = (self.n_neurons)
+        :return:
+        """
         output = []
         for i in range(len(self.neurons)):
             output.append(self.neurons[i].forward_propagate(previous_layer_output))
@@ -105,8 +137,15 @@ class DenseLayer(Layer):
             return self.next_layer.forward_propagate(output)
 
     def back_propagate(self, error=None):
+        """
+        Back propagate algorithm, update delta for each neuron
+        :param error: difference of actual output of expected output, should be None if this is not the output layer
+        :return:
+        """
         delta_ = []
         if self.isOutput and error is not None:
+            # if the current layer is not the output layer, then we can calculate delta of the neurons based on diff of
+            # actual output and expected output
             for i in range(len(self.neurons)):
                 delta_neuron = self.neurons[i].back_propagate(err=error[i], isOutput=True)
                 delta_.append(delta_neuron)
@@ -114,6 +153,8 @@ class DenseLayer(Layer):
             self.previous_layer.back_propagate()
 
         elif not self.isOutput:
+            # if the current layer is somehow a hidden layer, then the delta is calculated based on weighted sum of delta
+            # from next layer
             for i in range(len(self.neurons)):
                 delta_neuron = self.neurons[i].back_propagate(delta_next=self.get_weighted_delta_sum(i))
                 delta_.append(delta_neuron)
@@ -134,6 +175,13 @@ class DenseLayer(Layer):
         self.previous_layer.update(lr=lr, momentum=momentum)
 
     def get_weighted_delta_sum(self, index):
+        """
+        get weighted sum of next layer delta based on index
+        the weighted sum is calculated as the weighted sum of the synaptic weight of index th neuron of current layer to
+        each neuron of next layer and their delta
+        :param index: int; index of a neuron inside this layer
+        :return: float; weighted sum of delta
+        """
         if self.isOutput:
             return
         sum = 0
@@ -143,30 +191,137 @@ class DenseLayer(Layer):
 
 
 class InputLayer(Layer):
+    """
+    InputLayer object
+    """
+
     def __init__(self, n_neurons):
+        """
+
+        :param n_neurons: dimensionality of input
+        """
         Layer.__init__(self, n_neurons)
         self.next_layer = None
 
     def forward_propagate(self, input):
+        """
+        forward_propagate
+        To do a prediction using the current model, should invoke forward_propagate at the InputLayer
+        Then the calculation is completed recursively
+        :param input: np.array; input data, should have the same dimensionality with self.n_neurons
+        :return: np.array; output of the output layer, should have same dimensionality with output layer's n_neurons
+        """
         return self.next_layer.forward_propagate(input)
 
     def back_propagate(self):
+        """
+        back_propagate
+        Back propagate function for each layer. For InputLayer is just a sign of termination of back_propagate.
+        This function should only be called as a termination step of recursive execution of back_propagate by the next_layer
+        of this InputLayer
+        :return:
+        """
         return
 
     def update(self, lr=0.5, momentum=0.9):
+        """
+        update
+        Weight update function for each layer. For InputLayer is just a sign of termination of update call stack.
+        This function should only be called as a termination step of recursive execution of update by the next_layer
+        of this InputLayer
+        :param lr:
+        :param momentum:
+        :return:
+        """
         return
 
 
+class Model:
+    """
+    Model object that wraps the layers and provide model level functions
+    """
+
+    def __init__(self, input_layer, output_layer):
+        self.input_layer = input_layer
+        self.output_layer = output_layer
+
+    def err_(self, actual_output, desired_output):
+        """
+        Calculate difference of actual output and desired output
+        which is desired_output - actual output
+        :param actual_output: np.array; actual_output
+        :param desired_output: np.array; desired output
+        :return: np.array; with same dimensionality as desired_output
+        """
+        return np.subtract(desired_output, actual_output)
+
+    def abs_err_(self, actual_ouput, desired_output):
+        """
+        Calculate difference of actual output and desired output
+        which is abs(desired_output - actual output)
+        :param actual_ouput: np.array; actual output of the neural net
+        :param desired_output: np.array; desired output
+        :return: np.array; with same dimensionality as desired_output
+        """
+        return np.absolute(np.subtract(desired_output, actual_ouput))
+
+    def fit(self, Xs, Ys, lr=0.5, momentum=0.9, max_epochs=10000, tolerance=0.05):
+        """
+        Fit model on a set of data, this is not batch optimization
+        :param Xs: np.array, set of attributes
+        :param Ys: np.array, set of desired output
+        :param lr: learning rate
+        :param momentum: momentum
+        :param max_epochs: maximum epochs that limits the training iteration
+        :param tolerance: tolerance of early stopping
+        :return: list; log of average absolute difference of desired of output and actual output
+        """
+        if Xs.shape[0] != Ys.shape[0]:
+            return
+        log = []
+        for epoch in range(max_epochs):
+            for i in range(Xs.shape[0]):
+                # loop for updating weights
+                x = Xs[i, :]
+                y = Ys[i, :]
+                y_ = self.input_layer.forward_propagate(input=x)
+                err = self.err_(actual_output=y_, desired_output=y)
+                self.output_layer.back_propagate(error=err)
+                self.output_layer.update(lr=lr, momentum=momentum)
+            early_stop = True
+            loss_sum = np.zeros(Ys[0,:].shape)
+            for i in range(Xs.shape[0]):
+                # loop for early stop
+                x = Xs[i, :]
+                y = Ys[i, :]
+                y_ = self.input_layer.forward_propagate(input=x)
+                abs_error = self.abs_err_(actual_ouput=y_, desired_output=y)
+                loss_sum += abs_error
+                if np.greater(abs_error, tolerance).any():
+                    early_stop = False
+            loss_sum /= Ys.shape[0]
+            log.append(loss_sum)
+            if early_stop:
+                break
+
+        return log
+
+    def predict(self, Xs):
+        result = []
+        for i in range(Xs.shape[0]):
+            result.append(self.input_layer.forward_propagate(Xs[i, :]))
+        return np.array(result)
+
+
+# sample test code
 # input = InputLayer(3)
 # hid1 = DenseLayer(5, input)
-# hid2 = DenseLayer(3, hid1, isOutput=True)
-# input_value = np.random.rand(3)
-# a0 = input.forward_propagate(input_value)
-# for i in range(100):
-#     a = input.forward_propagate(input_value)
-#     diff = np.array([1,1,0])-a
-#     hid2.back_propagate(diff)
-#     hid2.update()
-#     b = input.forward_propagate(input_value)
-# print(a0)
-# print(b)
+# hid2 = DenseLayer(2, hid1, isOutput=True)
+#
+# mdl = Model(input, hid2)
+#
+# input_value = np.random.rand(2, 3)
+# desired_output = np.array([[1, 0], [1, 1]])
+# log = mdl.fit(Xs=input_value, Ys=desired_output)
+# print(mdl.predict(input_value))
+# print(log)
